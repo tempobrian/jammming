60058f8d01eb95138ea604be9eb2feedadd12981
Brian Pena <mr.penabrian@hotmail.com>
2023-09-04 12:23:21 -0400

    add state variables

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 function App() {
-    
+  const [selectedSongIds, setSelectedSongIds] = useState([]);
+  const [searchKey, setSearchKey] = useState("")
 }
 
 export default App;
