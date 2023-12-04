ef025046bdf3fd16a0a95de79b55335ed0cad952
Brian Pena <mr.penabrian@hotmail.com>
2023-12-04 17:17:41 -0500

    add image and headline

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import React, {useEffect, useState} from "react";
 function App() {
   const [selectedSongIds, setSelectedSongIds] = useState([]);
   const [searchKey, setSearchKey] = useState("")
+
+  return (
+    <div class='background-image'>
+      <h1 class="headline">Jam🎶ming</h1>
+    </div>
+    
+  )
 }
 
 export default App;
